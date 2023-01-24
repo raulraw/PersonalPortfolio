3c52f25a3ede187751f8722dd94680f0a3621132
raulraw <119895675+raulraw@users.noreply.github.com>
2023-01-24 13:35:35 +0200

    Update contact_db.sql

--- Personal Portfolio/contact_db.sql
+++ Personal Portfolio/contact_db.sql
@@ -1,22 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Mar 19, 2022 at 10:25 AM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Database: `contact_db`
 --
@@ -40,11 +25,7 @@ CREATE TABLE `contact_form` (
 --
 
 INSERT INTO `contact_form` (`id`, `name`, `email`, `number`, `message`) VALUES
-(5, 'shaikh anas', 'shaikh@gmail.com', '1234567890', 'hi, how are you?');
-
---
--- Indexes for dumped tables
---
+(5, 'Raul Staicu', 'raul_valentin123@yahoo.com', '1234567890', 'Hey there sweetie, are you watching?');
 
 --
 -- Indexes for table `contact_form`
@@ -62,7 +43,3 @@ ALTER TABLE `contact_form`
 ALTER TABLE `contact_form`
   MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
